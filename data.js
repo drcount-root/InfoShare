2c9d888c899e1421a11ae0d9ed8df29aefe6b0ff
Subham Sahu <about.subhamsahu@gmail.com>
2022-12-14 09:42:44 +0530

    14th Dec 2022 - v36

--- data.js
+++ data.js
@@ -46,3 +46,4 @@ const initialFacts = [
 // LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
 
 // 👍 🤯 ⛔️
+
---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
